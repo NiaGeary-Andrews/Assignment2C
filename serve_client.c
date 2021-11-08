d6f667e3e7497b9bca5fc226bdd9f886f325637c
NiaGeary-Andrews <74965796+NiaGeary-Andrews@users.noreply.github.com>
2021-11-08 17:55:09 +0000

    Reading from file works

--- serve_client.c
+++ serve_client.c
@@ -1,24 +1,65 @@
+#include<pthread.h>
 
 pthread_rwlock_t lock = PTHREAD_RWLOCK_INITIALIZER;
+void* ServerCLien(char *client);
+int counter =0;
 
 void *downtime(){
 
 	sleep(1);
 	//TODO: 1st downtime: Do balanceTree here
+	//balanceTree();
 
 	sleep(1);
 	//TODO: 2nd downtime: Do balanceTree here
+	//balanceTree();
 
 	sleep(1);
 	//TODO: 3rd downtime: Do balanceTree here
+	//balanceTree();
 
 	return NULL;
 }
 
 
 void* ServeClient(char *client){
+	
+	char ch;
+	FILE *fp;
 
+	//---------------------------------------------------
+	// This part opens a file and reads line by line the commands, think it just needs to be opened for reading though. Make sure to lock it to stop concurrency issues
 	// TODO: Open the file and read commands line by line
+	
+	lock(&mutex1);
+	counter++;
+	//opens file ready for reading specified by the parameter input
+	fp = fopen(client,"r");
+	
+	//reads one char from file
+	ch = getc(fp);
+	//while the char is not equal to the end of the file execute the loop
+	while(ch != EOF){
+		switch(ch) {
+			//case "insertNode":
+			//printf(client+ch+ theINteger);
+			
+			//case "deleteNode":
+			
+			//case "countNodes":
+			
+			//case "sumSubtree":
+			
+			default:
+				printf("Invalid command");
+		}
+		printf("%d",ch);
+		ch = getc(fp);
+	}
+	
+	//printf("%s ",client);
+	//printf("%d ", ch);
+	//printf("\n");
 
 	// TODO: match and execute commands
 
@@ -39,6 +80,6 @@ void* ServeClient(char *client){
 	// print: "[ClientName]sumSubtree = <SomeNumber>\n"
 	// e.g. "[client1_commands]sumSubtree 1\n"
 
-
+	fclose(fp);
 	return NULL;
 }
