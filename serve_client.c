da4dad6a1b1fd95f21f36b31f3669c70ad5b4fdc
NiaGeary-Andrews <74965796+NiaGeary-Andrews@users.noreply.github.com>
2021-11-08 18:41:17 +0000

    Add files via upload
    Reading file works and I think I have sorted out the locking system for read/writing

--- serve_client.c
+++ serve_client.c
@@ -1,8 +1,7 @@
 #include<pthread.h>
 
 pthread_rwlock_t lock = PTHREAD_RWLOCK_INITIALIZER;
-void* ServerCLien(char *client);
-int counter =0;
+//pthread_rwlock_init(lock, NULL);
 
 void *downtime(){
 
@@ -21,39 +20,58 @@ void *downtime(){
 	return NULL;
 }
 
+//commands:
+//Locking a resource for writing using the function --------------------------------------------
+//int pthread_rwlock_wrlock(pthread_rwlock_t *rwlock);
+//Unlock the resource for writing using the function -------------------------------------------
+//int pthread_rwlock_unlock(pthread_rwlock_t *rwlock);
+//To lock the resource only for reading and not writing we can use the function ----------------
+//int pthread_rwlock_rdlock(pthread_rwlock_t *rwlock);
+//The lock for reading can be unlocked by the function------------------------------------------
+//int pthread_rwlock_rdlock(pthread_rwlock_t *rwlock);
 
 void* ServeClient(char *client){
 	
+	//locks for writing so only one client can write at a time
+	int pthread_rwlock_wrlock(pthread_rwlock_t *lock);
 	char ch;
 	FILE *fp;
+	//char *split;
 
 	//---------------------------------------------------
 	// This part opens a file and reads line by line the commands, think it just needs to be opened for reading though. Make sure to lock it to stop concurrency issues
 	// TODO: Open the file and read commands line by line
-	
-	lock(&mutex1);
-	counter++;
 	//opens file ready for reading specified by the parameter input
 	fp = fopen(client,"r");
 	
 	//reads one char from file
 	ch = getc(fp);
+	//split = strtok(ch, " ");
 	//while the char is not equal to the end of the file execute the loop
 	while(ch != EOF){
-		switch(ch) {
-			//case "insertNode":
+		/*switch(split[0]) {
+			case "insertNode":
 			//printf(client+ch+ theINteger);
+				printf("InsertNode");
+				break;
 			
-			//case "deleteNode":
+			case "deleteNode":
+				printf("DeleteNode");
+				break;
 			
-			//case "countNodes":
+			case "countNodes":
+				printf("countNodes");
+				break;
 			
-			//case "sumSubtree":
+			case "sumSubtree":
+				printf("sumSubtree");
+				break;
 			
 			default:
 				printf("Invalid command");
-		}
-		printf("%d",ch);
+		}*/
+		printf("%s",client);
+		printf("%c",ch);
 		ch = getc(fp);
 	}
 	
@@ -81,5 +99,7 @@ void* ServeClient(char *client){
 	// e.g. "[client1_commands]sumSubtree 1\n"
 
 	fclose(fp);
+	//when finished it unlocks for writing
+	int pthread_rwlock_unlock(pthread_rwlock_t *lock);
 	return NULL;
 }
